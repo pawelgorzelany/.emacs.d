a48149971abf9666f828a2fc485cdc0247e8abc0
Pawel Gorzelany <pawel.gorzelany@gmail.com>
2014-07-06 20:51:48 +0200

    basic emacs setup with neon theme

---
+++ init.el
@@ -0,0 +1,56 @@
+;;; init.el --- Initialization file for emacs.
+;;; Commentary:
+
+;;; Code:
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
+ '(custom-safe-themes (quote ("bb452baeed77ebb3dbd7d87df64fdc27cd9cbae868bcc25eee197df17298cfb2" default)))
+ '(global-linum-mode t)
+ '(inhibit-startup-screen t)
+ '(linum-format "%4d ")
+ '(make-backup-files nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; use a custom color theme
+(load-theme 'neon)
+
+;; initialize MELPA
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+;; initialize exec-path-from-shell
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;; initialize slime
+(require 'slime-autoloads)
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+(setq slime-contribs '(slime-fancy))
+
+;; initialize flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; shrten 'yes or no' to 'y or n'
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(provide 'init)
+;;; init.el ends here
+
---
+++ neon-theme.el
@@ -0,0 +1,60 @@
+;; based on color-theme-phil-neon.el
+
+(deftheme neon
+ "neon-theme")
+
+(custom-theme-set-faces
+ 'neon
+
+ '(default ((t (:background "#000000" :foreground "#f8f8f2"))))
+ '(mouse ((t (:foreground "black"))))
+ '(cursor ((t (:foreground "green"))))
+ '(border ((t (:foreground "white"))))
+
+ '(bold ((t (:bold t :foreground "#ffffff"))))
+ '(bold-italic ((t (:italic t :bold t))))
+ '(calendar-today-face ((t (:underline t))))
+ '(diary-face ((t (:foreground "red"))))
+ '(font-lock-builtin-face ((t (:bold t :foreground "#66d9ef"))))
+ '(font-lock-comment-face ((t (:foreground "#8e8e8e"))))
+ '(font-lock-constant-face ((t (:foreground "#f8f8f2"))))
+ '(font-lock-function-name-face ((t (:foreground "#a4e402"))))
+ '(font-lock-keyword-face ((t (:foreground "#f92672"))))
+ '(font-lock-string-face ((t (:foreground "#e6dc6d"))))
+ '(font-lock-type-face ((t (:foreground "#66d9ef"))))
+ '(font-lock-variable-name-face ((t (:foreground "#fd971f"))))
+ '(font-lock-warning-face ((t (:bold t :foreground "gold"))))
+ '(highlight ((t (:background "darkslategrey" :foreground "wheat"))))
+ '(holiday-face ((t (:background "pink"))))
+ '(info-menu-5 ((t (:underline t))))
+ '(info-node ((t (:italic t :bold t))))
+ '(info-xref ((t (:bold t))))
+ '(italic ((t (:italic t :background "gray"))))
+ '(message-cited-text-face ((t (:foreground "red"))))
+ '(message-header-cc-face ((t (:bold t :foreground "green4"))))
+ '(message-header-name-face ((t (:foreground "DarkGreen"))))
+ '(message-header-newsgroups-face ((t (:italic t :bold t :foreground "yellow"))))
+ '(message-header-other-face ((t (:foreground "#b00000"))))
+ '(message-header-subject-face ((t (:foreground "green3"))))
+ '(message-header-to-face ((t (:bold t :foreground "green2"))))
+ '(message-header-xheader-face ((t (:foreground "blue"))))
+ '(message-mml-face ((t (:foreground "ForestGreen"))))
+ '(message-separator-face ((t (:foreground "blue3"))))
+ '(modeline ((t (:background "white" :foreground "DeepSkyBlue3"))))
+ '(modeline-buffer-id ((t (:background "white" :foreground "DeepSkyBlue3"))))
+ '(modeline-mousable ((t (:background "white" :foreground "DeepSkyBlue3"))))
+ '(modeline-mousable-minor-mode ((t (:background "white" :foreground "DeepSkyBlue3"))))
+ '(region ((t (:background "grey"))))
+ '(secondary-selection ((t (:background "darkslateblue"))))
+ '(show-paren-match-face ((t (:background "turquoise"))))
+ '(show-paren-mismatch-face ((t (:background "purple" :foreground "white"))))
+ '(underline ((t (:underline t)))))
+
+;; auto load
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
+(provide-theme 'neon)
+
+;; neon theme ends here
