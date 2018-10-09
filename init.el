614edd9c453a1a3e96ee4747ed4e5addfde960f1
Paweł Gorzelany <pawel.gorzelany@tooploox.com>
2018-10-09 14:42:38 +0200

    removed tide and jedi, cleanups and fixes after package updates

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;;; init.el --- Initialization file for Emacs.
 ;;; Commentary:
 ;;; This is my Emacs setup.
-;;; author: @gonzomember
+;;; author: @pawelgorzelany
 
 ;;; Code:
 
@@ -22,7 +22,7 @@
  '(org-log-done t)
  '(package-selected-packages
    (quote
-    (intero yaml-mode web-mode virtualenvwrapper use-package twittering-mode tide slime rainbow-delimiters nyan-mode moe-theme markdown-mode magit literate-coffee-mode json-mode jedi helm-projectile haskell-mode exec-path-from-shell elpy elm-mode column-enforce-mode cider)))
+    (projectile helm company flycheck yasnippet-snippets intero yaml-mode web-mode virtualenvwrapper use-package twittering-mode tide slime rainbow-delimiters nyan-mode moe-theme markdown-mode magit literate-coffee-mode json-mode jedi helm-projectile haskell-mode exec-path-from-shell elpy elm-mode column-enforce-mode cider)))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
@@ -40,7 +40,7 @@
 
 ;; initialize MELPA
 ;; by default install all packages from melpa but add also melpa-stable for pinned packages
-;; like for example cider
+;; like for example cider and slime
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
@@ -48,118 +48,92 @@
 
 (package-initialize)
 
-;; here is a list of all pinned packages that should be downloaded from melpa-stable
-(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
-
 ;; setup `use-package` package, install if not previously installed
-(if (not (package-installed-p 'use-package))
+(unless (package-installed-p 'use-package)
     (progn
       (package-refresh-contents)
       (package-install 'use-package)))
 
-(require 'use-package)
+(eval-when-compile
+  (require 'use-package))
 
 ;; now setup all 3rd party packages and install them as necessary
 
-;; themes - uncomment which ever suits the mood
-
-;; (use-package monokai-theme
-;;   :ensure t
-;;   :config (progn
-;;             (load-theme 'monokai t)))
-
 (use-package moe-theme
   :ensure t
-  :config (progn
-            (load-theme 'moe-dark t)))
-
-;; themes selection ends here.
+  :config (load-theme 'moe-dark t))
 
 (use-package column-enforce-mode
   :ensure t
-  :init (progn
-          (global-column-enforce-mode t)))
+  :init (global-column-enforce-mode t))
 
 (use-package rainbow-delimiters
   :ensure t
-  :init (progn
-          ;; set hooks for specific modes
-          ;; for all programming modes
-          (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)))
-          ;; or for python only
-          ;; (add-hook 'python-mode-hook 'rainbow-delimiters-mode)
-          ;; or if shit gets fancy set rainbowish flavor all over the place
-          ;; (global-rainbow-delimiters-mode)))
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package flycheck
   :ensure t
-  :init (progn
-          (add-hook 'after-init-hook #'global-flycheck-mode)))
+  :hook (after-init . global-flycheck-mode))
 
 (use-package company
   :ensure t
-  :init (progn
-          (add-hook 'after-init-hook 'global-company-mode)))
-
-;; (use-package auto-complete
-;;   :ensure t
-;;   :init (progn
-;;           (ac-config-default)))
+  :hook (after-init . global-company-mode))
 
 (use-package elpy
   :ensure t
-  :init (progn
-          (elpy-enable)
-          (setq elpy-rpc-backend "jedi")
-          (setq elpy-modules
-                '(elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-yasnippet elpy-module-sane-defaults))))
+  :init (elpy-enable)
+  :config (setq elpy-modules '(elpy-module-company
+                               elpy-module-eldoc
+                               elpy-module-pyvenv
+                               elpy-module-yasnippet
+                               elpy-module-sane-defaults)))
+
+(use-package yasnippet-snippets
+  :ensure t)
 
 (use-package yasnippet
+  :after yasnippet-snippets
   :ensure t
-  :init (progn
-          ;; make yasnippet globally available
-          (yas-global-mode t)
-          ;; but disable it in shell mode
-          (add-hook 'shell-mode-hook #'(lambda ()
-                                         (yas-minor-mode nil)))))
+  :hook (prog-mode . yas-minor-mode)
+  :init (yas-reload-all))
 
 (use-package helm
   :ensure t
-  :init (progn
-          (setq default-directory "~/Projects/")
-          (helm-mode t)
-          (bind-key "C-c h" 'helm-mini)))
+  :bind ("C-c h" . helm-mini)
+  :init (helm-mode t)
+  :config (setq default-directory "~/Projects/"))
 
 (use-package projectile
   :ensure t
-  :init (progn
-          (use-package helm-projectile
-            :ensure t)
-          (projectile-mode)
-          (setq projectile-completion-system 'helm)
-          (helm-projectile-on)))
+  :after helm
+  :bind-keymap ("C-c p" . projectile-command-map)
+  :init (projectile-mode t)
+  :config (setq projectile-completion-system 'helm))
+
+(use-package helm-projectile
+  :ensure t
+  :after (helm projectile)
+  :init (helm-projectile-on))
 
 (use-package slime
+  :pin melpa-stable
   :ensure t
   :commands slime
-  :init (progn
-            ;this works well for OS X and brew but on linux usually requires a symlink
-            (setq inferior-lisp-program (executable-find "sbcl"))
-            (setq slime-contribs '(slime-scratch slime-editing-commands))
-            (setq slime-contribs '(slime-fancy))))
+  :init
+  (setq inferior-lisp-program (executable-find "sbcl"))
+  (setq slime-contribs '(slime-fancy)))
 
 (use-package cider
+  :pin melpa-stable
   :ensure t)
 
 (use-package haskell-mode
   :ensure t
-  :init (progn
-          (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)))
+  :hook (haskell-mode . turn-on-haskell-indent))
 
 (use-package intero
   :ensure t
-  :init (progn
-          (add-hook 'haskell-mode-hook 'intero-mode)))
+  :hook (haskell-mode . intero-mode))
 
 (use-package markdown-mode
   :ensure t
@@ -174,72 +148,29 @@
 (use-package elm-mode
   :ensure t)
 
-(defun setup-tide-mode ()
-  "Setup tide for TypeScript hacking."
-  (interactive)
-  (tide-setup)
-  (flycheck-mode t)
-  (setq flycheck-check-syntax-automatically '(idle-change mode-enabled))
-  (eldoc-mode t))
-  ;; company is an optional dependency. You have to
-  ;; install it separately via package-install
-  ;; `M-x package-install [ret] company`
-  ;; (company-mode +1))
-
-(use-package tide
-  :ensure t
-  :mode (("\\.tsx\\'" . web-mode) ("\\.jsx\\'" . web-mode))
-  :init (progn
-          (add-hook 'web-mode-hook
-                    (lambda ()
-                      (when (or
-                             (string-equal "tsx" (file-name-extension buffer-file-name))
-                             (string-equal "jsx" (file-name-extension buffer-file-name)))
-                        (setup-tide-mode))))
-          ;; aligns annotation to the right hand side
-          ;; (setq company-tooltip-align-annotations t)
-
-          ;; formats the buffer before saving
-          (add-hook 'before-save-hook 'tide-format-before-save)
-          (add-hook 'typescript-mode-hook #'setup-tide-mode)
-
-          ;; format options
-          (setq tide-format-options '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t :placeOpenBraceOnNewLineForFunctions nil))
-          (setq tide-tsserver-executable "node_modules/typescript/bin/tsserver")))
-
-
 (use-package web-mode
   :ensure t
   :mode (("\\.html?\\'" . web-mode) ("\\.php\\'" . web-mode))
-  :init (progn
-          (setq web-mode-markup-indent-offset 2)))
+  :config (setq web-mode-markup-indent-offset 2))
 
 (use-package magit
   :ensure t
   :bind ("C-c m" . magit-status))
 
-;; (use-package jedi
-;;   :ensure t
-;;   :init (progn
-;;           (add-hook 'python-mode-hook 'jedi:setup)
-;;           (setq jedi:complete-on-dot t)))
-
 (use-package virtualenvwrapper
   :ensure t
-  :init (progn
-          (setq venv-location "~/.virtualenvs/")))
+  :config (setq venv-location "~/.virtualenvs/"))
 
 (use-package twittering-mode
   :ensure t
-  :init (progn
-          (setq twittering-use-master-password t)
-          (setq twittering-icon-mode t)
-          (setq twittering-use-icon-storage t)))
+  :config
+  (setq twittering-use-master-password t)
+  (setq twittering-icon-mode t)
+  (setq twittering-use-icon-storage t))
 
 (use-package nyan-mode
   :ensure t
-  :init (progn
-          (nyan-mode t)))
+  :init (nyan-mode t))
 
 ;; do some custom OS X stuff like:
 ;;; - initialize exec-path-from-shell
@@ -249,8 +180,7 @@
     (progn
       (use-package exec-path-from-shell
         :ensure t
-        :init (progn
-                (exec-path-from-shell-initialize)))
+        :config (exec-path-from-shell-initialize))
       (menu-bar-mode t)
       (setq mac-option-key-is-meta t)
       (setq mac-right-option-modifier nil)))
@@ -264,11 +194,11 @@
                                              (abbreviate-file-name (buffer-file-name))
                                            "%b"))))))
 
-(add-hook 'window-setup-hook 'set-window-title)
+(add-hook 'window-setup-hook #'set-window-title)
 
 ;; trim trailing whitespace and delete blank lines on save
-(add-hook 'before-save-hook 'delete-trailing-whitespace
-          'delete-blank-lines)
+(add-hook 'before-save-hook #'delete-trailing-whitespace
+          #'delete-blank-lines)
 
 ;; shorten 'yes or no' to 'y or n'
 (defalias 'yes-or-no-p 'y-or-n-p)
